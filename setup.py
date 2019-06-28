6fab3d04679a9477e242b785a2811b626508aa73
Scott Stafford <scott.stafford@gmail.com>
2019-06-28 15:03:46 -0400

    Update version to 1.3.0.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="django-bulk-sync",
-    version='1.2.0',
+    version='1.3.0',
     description="Combine bulk add, update, and delete into a single call.",
     long_description=long_description,
     long_description_content_type='text/markdown',
