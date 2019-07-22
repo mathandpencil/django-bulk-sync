bc448926f0caecc312a09a1e7ffa5ae65b7c1289
Scott Stafford <scott.stafford@gmail.com>
2019-07-22 15:53:58 -0400

    Fix to update test models with Django 2.0 support. #3

--- tests/models.py
+++ tests/models.py
@@ -10,7 +10,7 @@ class Employee(models.Model):
     age = models.IntegerField()
     name = models.CharField(max_length=140, blank=True, null=True)
 
-    company = models.ForeignKey(Company)
+    company = models.ForeignKey(Company, models.CASCADE)
 
     def __str__(self):
         return "Employee: {} age {} company {}".format(self.name, self.age, self.company_id)
