3f8bc6670c19ed8534d6c90a5a5410bdef1a3fa8
Scott Stafford <scott.stafford@gmail.com>
2020-08-05 10:30:01 -0400

    Transactions now support models routed to different databases.

--- bulk_sync/__init__.py
+++ bulk_sync/__init__.py
@@ -1,33 +1,45 @@
 from collections import OrderedDict
 import logging
-from django.db import transaction
+from django.db import transaction, router
 
 logger = logging.getLogger(__name__)
 
 
-def bulk_sync(new_models, key_fields, filters, batch_size=None, fields=None, skip_creates=False, skip_updates=False, skip_deletes=False):
+def bulk_sync(
+    new_models,
+    key_fields,
+    filters,
+    batch_size=None,
+    fields=None,
+    skip_creates=False,
+    skip_updates=False,
+    skip_deletes=False,
+):
     """ Combine bulk create, update, and delete.  Make the DB match a set of in-memory objects.
 
     `new_models`: Django ORM objects that are the desired state.  They may or may not have `id` set.
     `key_fields`: Identifying attribute name(s) to match up `new_models` items with database rows.  If a foreign key
             is being used as a key field, be sure to pass the `fieldname_id` rather than the `fieldname`.
     `filters`: Q() filters specifying the subset of the database to work in.  Use `None` or `[]` if you want to sync against the entire table.
-    `batch_size`: passes through to Django `bulk_create.batch_size` and `bulk_update.batch_size`, and controls
+    `batch_size`: (optional) passes through to Django `bulk_create.batch_size` and `bulk_update.batch_size`, and controls
             how many objects are created/updated per SQL query.
     `fields`: (optional) list of fields to update. If not set, will sync all fields that are editable and not auto-created.
     `skip_creates`: If truthy, will not perform any object creations needed to fully sync. Defaults to not skip.
-    `skip_updates`: If truthy, will not perform any object updates needed to fully sync. Defaults to not skip. 
-    `skip_deletes`: If truthy, will not perform any object deletions needed to fully sync. Defaults to not skip. 
+    `skip_updates`: If truthy, will not perform any object updates needed to fully sync. Defaults to not skip.
+    `skip_deletes`: If truthy, will not perform any object deletions needed to fully sync. Defaults to not skip.
     """
     db_class = new_models[0].__class__
 
     if fields is None:
         # Get a list of fields that aren't PKs and aren't editable (e.g. auto_add_now) for bulk_update
-        fields = [field.name
-                  for field in db_class._meta.fields
-                  if not field.primary_key and not field.auto_created and field.editable]
+        fields = [
+            field.name
+            for field in db_class._meta.fields
+            if not field.primary_key and not field.auto_created and field.editable
+        ]
 
-    with transaction.atomic():
+    using = router.db_for_write(db_class)
+    with transaction.atomic(using=using):
         objs = db_class.objects.all()
         if filters:
             objs = objs.filter(filters)
@@ -51,7 +63,7 @@ def bulk_sync(new_models, key_fields, filters, batch_size=None, fields=None, ski
 
         if not skip_creates:
             db_class.objects.bulk_create(new_objs, batch_size=batch_size)
-            
+
         if not skip_updates:
             db_class.objects.bulk_update(existing_objs, fields=fields, batch_size=batch_size)
 
@@ -62,9 +74,9 @@ def bulk_sync(new_models, key_fields, filters, batch_size=None, fields=None, ski
         assert len(existing_objs) == len(new_models) - len(new_objs)
 
         stats = {
-            "created": 0 if skip_creates else len(new_objs), 
-            "updated": 0 if skip_updates else (len(new_models) - len(new_objs)), 
-            "deleted": 0 if skip_deletes else len(obj_dict)
+            "created": 0 if skip_creates else len(new_objs),
+            "updated": 0 if skip_updates else (len(new_models) - len(new_objs)),
+            "deleted": 0 if skip_deletes else len(obj_dict),
         }
 
         logger.debug(
--- setup.py
+++ setup.py
@@ -5,20 +5,20 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="django-bulk-sync",
-    version='3.0.0',
+    version="3.0.1",
     description="Combine bulk add, update, and delete into a single call.",
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     url="https://github.com/mathandpencil/django-bulk-sync",
     author="Scott Stafford",
     author_email="scott.stafford+bulksync@gmail.com",
     packages=setuptools.find_packages(),
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Framework :: Django',
-        'Framework :: Django :: 2.2',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Framework :: Django :: 2.2",
     ],
     zip_safe=False,
 )
